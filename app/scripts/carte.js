6707ed4ac32b408869870648182f260a48c1ffc7
henriB <henri.bretseau@gmail.com>
2019-05-28 11:55:19 +0200

    Function afficher réservation dépendante du click

--- app/scripts/carte.js
+++ app/scripts/carte.js
@@ -12,19 +12,21 @@ var oXhr = new XMLHttpRequest();
 oXhr.onload = function () {
     var data = JSON.parse(this.responseText);
     // ici les données sont exploitables
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         // personalisation marqueur
-        var myIcon = L.icon({
+        let myIcon = L.icon({
             iconUrl: '../images/station.png',
             iconSize: [25, 32],
-            
-            popupAnchor: [-3, -76]
         });
-        var marker = L.marker([data[i].position.lat, data[i].position.lng],{ icon: myIcon }).addTo(map);
-        marker.bindPopup('<h5>' + data[i].name + '</h5> <p>' + data[i].address + ' </p> <p> Station : ' + data[i].status + '</p><p> Total places : ' + data[i].bike_stands + '</p>');
-
-        afficherRéservation(i);
+        let marker = L.marker([data[i].position.lat, data[i].position.lng], { icon: myIcon }).addTo(map);
+        marker.bindPopup('<h5>' + data[i].name + '</h5> <p>' + data[i].address + ' </p> <p> Etat de station : ' + data[i].status + '</p><p> Total places : ' + data[i].bike_stands + '</p>');
         
+        
+        marker.addEventListener("click", function() {
+            afficherRéservation(i);
+            });
+        
+
     }
     function afficherRéservation(i) {
         var adresseElt = document.querySelector('.adresse');
@@ -41,22 +43,3 @@ oXhr.onerror = function (data) {
 };
 oXhr.open('GET', url, true);
 oXhr.send(null);
-/*var requestURL ='../json/lyon.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-    var  coord= request.response;
-
-    for ( var i=0; i<coord.length;i++){
-        var marker =L.marker([coord[i].latitude,coord[i].longitude]).addTo(map);
-        marker.bindPopup('<h5>'+coord[i].name+'</h5> <p>'+coord[i].address+' </p>');
-    }
-  }*/
-
-
-
-
-
-var marker = L.marker(lyon, { icon: myIcon }).addTo(map);
