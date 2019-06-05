e7b1327611f53b4a2507df617e0af49546e4f44c
henriB <henri.bretseau@gmail.com>
2019-06-05 15:08:00 +0200

    check input format POO

--- app/scripts/session.js
+++ app/scripts/session.js
@@ -1,41 +1,47 @@
 class SessionStorage {
     constructor() {
+        this.nom = document.getElementById('nom');
+        this.prenom = document.getElementById('prenom');
+        this.reserverElt = document.getElementById('reserver');
+        this.missNom = document.getElementById('missNom');
+        this.formatNom = document.getElementById('formatNom');
+        this.formatPrenom = document.getElementById('formatPrenom');
         this.checkInput();
+        //this.stocker();
+        //this.lecture();
+        //this.afficher();
     }
-    checkInput() { // Vérifier Nom et Prénom présent + Format "NOM" et "Prénom"
-        var reserver = document.getElementById('reserver');
-        var nom = document.getElementById('nom');
-        var prenom = document.getElementById('prenom');
-        var missNom = document.getElementById('missNom');
-        var formatNom = document.getElementById('formatNom');
-        var formatPrenom = document.getElementById('formatPrenom');
-        var nomValid = /^[zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+([-'\s][zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+)?$/;
-        var prenomValid = /^[zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-
-        reserver.addEventListener('click', validation);
+    checkInput() { // Vérifier Nom et Prénom présent + Format "NOM" et "Prénom"        
+        var nomValid = /^[zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+([-'\s][zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+)?$/; //REGEX [BRESTEAU]
+        var prenomValid = /^[zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/; //REGEX [Henri]
+        this.reserverElt.addEventListener('click', validation);
+        var self = this;
         function validation(event) {
+
             //Si le champ est vide
-            if (nom.validity.valueMissing || prenom.validity.valueMissing) {
+            if (self.nom.validity.valueMissing || self.prenom.validity.valueMissing) {
                 event.preventDefault();
-                missNom.textContent = 'Attention ! Nom et prenom sont obligatoire pour réserver !';
-                missNom.style.color = 'red';
+                self.missNom.textContent = 'Attention ! Nom et prenom sont obligatoire pour réserver !';
+                self.missNom.style.color = 'red';
+            } else {
+                self.missNom.textContent = "";
             }
-            if (nomValid.test(nom.value) == false) {
+            if (nomValid.test(self.nom.value) == false) {
                 event.preventDefault();
                 //formatNom.textContent += 'Format incorrect';
-                formatNom.style.color = 'orange';
+                self.formatNom.style.color = 'orange';
+            } else {
+                self.formatNom.style.color = 'green';
             }
-            if (prenomValid.test(prenom.value) == false) {
+            if (prenomValid.test(self.prenom.value) == false) {
                 event.preventDefault();
-                //formatNom.textContent += 'Format incorrect';
-                formatPrenom.style.color = 'orange';
+                self.formatPrenom.style.color = 'orange';
             }
             else {
-                formatNom.style.color = 'green';
-                formatPrenom.style.color = 'green';
-                missNom.textContent = "";
+                self.formatPrenom.style.color = 'green';
             }
         }
     }
+
 }
 var session = new SessionStorage();
