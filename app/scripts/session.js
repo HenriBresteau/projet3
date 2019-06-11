9394bce06f48c62c85cb0e0e0dc750bd05c66e9e
henriB <henri.bretseau@gmail.com>
2019-06-11 12:25:02 +0200

    Chronos

--- app/scripts/session.js
+++ app/scripts/session.js
@@ -8,8 +8,11 @@ class SessionStorage {
         this.formatNom = document.getElementById('formatNom');
         this.formatPrenom = document.getElementById('formatPrenom');
         this.recapElt = document.getElementById('recap');
+        this.minElt = document.getElementById('min');
+        this.secElt = document.getElementById('sec');
         this.checkInput();
         this.afficherResa();
+        this.chrono();
     }
     checkInput() { // Vérifier Nom et Prénom présent + Format "NOM" et "Prénom"        
         var nomValid = /^[zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+([-'\s][zA-ZéèîïÉÈÎÏ][zA-ZéèîïÉÈÎÏ]+)?$/; //REGEX [BRESTEAU]
@@ -51,16 +54,54 @@ class SessionStorage {
 
     }
     afficherResa() {
-        var that = this
+        var that = this;
         this.reserverElt.addEventListener("click", function (e) {
             that.recapElt.style.display = "initial";
             var stationReserver = document.getElementById('station');
-            stationReserver.textContent= that.station.textContent;
+            stationReserver.textContent = that.station.textContent;
             var nomResa = document.querySelector('#nomResa');
             nomResa.textContent = that.nom.value;
             var prenomResa = document.querySelector('#prenomResa');
             prenomResa.textContent = that.prenom.value;
         });
+    }
+    chrono() {
+        var that = this;
+        var counterSec = 10;
+        var counterMin = 1;
+        var intervalId = null;
+        var finElt = document.getElementById('fin');
+        finElt.style.display= "none";
+        function fin() {
+            clearInterval(intervalId);
+            finElt.style.display ="initial";
+            finElt.style.color ="red";
+        }
+        function bip() {
+            counterSec--;
+            if(counterSec <0){
+                counterSec = 10;
+                counterMin --;
+            }
+            if ((counterSec == 0) && (counterMin==0)) {
+                fin();
+            }             
+            else {
+                that.secElt.innerHTML = counterSec ;
+                that.minElt.innerHTML = counterMin;
+            }
+        }
+        this.reserverElt.addEventListener("click", function () {
+            intervalId = setInterval(bip, 1000);
+            that.reserverElt.addEventListener("click", function() {
+                clearInterval(intervalId);
+                counterMin = 1;
+                counterSec = 10;
+                finElt.style.display= "none";
+                intervalId = setInterval(bip, 1000);
+            });
+        });
+        
     }
 
 }
